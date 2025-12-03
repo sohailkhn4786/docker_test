ecc65ab4eb0f59358f4685ef19cc40db931a0089
sohail <91713757+sohailkhn4786@users.noreply.github.com>
2025-12-04 02:03:46 +0530

    Update HelloWorldController.java

--- src/main/java/com/demo/controller/HelloWorldController.java
+++ src/main/java/com/demo/controller/HelloWorldController.java
@@ -1,5 +1,13 @@
 package com.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
 
+    @GetMapping("/message")
+    public String message() {
+        return "Hello from Docker + Jenkins + AWS EC2!";
+    }
 }
